0425ee5e4e1818315c6ec7fcf1fb9d8f7af4cdea
Christian Stevens <stev1905@gmail.com>
2016-03-30 12:07:38 -0400

    application.js changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
 //= require bxslider
 //= require owl.carousel
+//= require bootstrap-sprockets
 //= require_tree .
